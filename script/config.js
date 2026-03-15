3891354539d707202494b7cf4158443ad746bbaa
Yoann Lemesle <yoannlemesle@gmail.com>
2026-03-15 18:00:09 +0100

    responsive total

--- script/config.js
+++ script/config.js
@@ -1,10 +1,9 @@
 /** Modification simplifiée des Asides */
 
-let annee_1_an ="2026" /** l'année en cours */
 let piece_titre ="Impair et Père (Ray Cooney)" /** Titre de la pièce en cours */
 let annee_2_ndt ="2026" /** l'année de la nuit du théatre en cours */
 let date_ndt ="Samedi 4 avril 2026" /** la date de la nuit du théatre en cours */
-let petite_piece_titre ="Moi je crois pas ! (Jean-Claude Grumberg)"
+let petite_piece_titre ="Moi je crois pas ! (Jean-Claude Grumberg)" /** la petite pièce */
 
 /**Application dans la page Web */
 
@@ -19,6 +18,3 @@ date.innerText = date_ndt
 
 let petite_piece = document.getElementById("petite_piece")
 petite_piece.innerText = petite_piece_titre
-
-let annee_1 = document.getElementById("annee_1")
-annee_1.innerText = annee_1_an
