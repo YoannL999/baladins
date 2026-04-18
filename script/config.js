5879c041d6e658bbb27d214e8e23fcdd79060fa0
Yoann Lemesle <yoannlemesle@gmail.com>
2026-04-18 18:59:47 +0200

    Modifications demandées par les Baladins (part 1)

--- script/config.js
+++ script/config.js
@@ -1,7 +1,7 @@
 /** Modification simplifiée des Asides */
 
 let piece_titre ="Impair et Père (Ray Cooney)" /** Titre de la pièce en cours */
-let annee_2_ndt ="2026" /** l'année de la nuit du théatre en cours */
+let annee_ndt ="2026" /** l'année de la nuit du théatre en cours */
 let date_ndt ="Samedi 4 avril 2026" /** la date de la nuit du théatre en cours */
 let petite_piece_titre ="Moi je crois pas ! (Jean-Claude Grumberg)" /** la petite pièce */
 
@@ -10,8 +10,8 @@ let petite_piece_titre ="Moi je crois pas ! (Jean-Claude Grumberg)" /** la petit
 let piece = document.getElementById("piece")
 piece.innerText = piece_titre
 
-let annee_2 = document.getElementById("annee_2")
-annee_2.innerText = annee_2_ndt
+let annee = document.getElementById("annee")
+annee.innerText = annee_ndt
 
 let date = document.getElementById("date")
 date.innerText = date_ndt
